7a13fa6ab56852c2496553e84dfe6ab4db0f392d
Rafaela <rafaelaaayres@gmail.com>
2025-03-15 22:06:13 -0300

    atualizações finais no route e database

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,7 +1,38 @@
 let mongoose = require('mongoose');
+const Admin = require('../model/adminModel.js')
 
 const server = 'localhost:27017'; 
 const database = 'modusDB'; // botar o nome do banco
+
+function criarAdminPadrao() {
+  Admin.find()
+    .then((admins) => {
+      if (admins.length === 0) { 
+        return bcrypt.hash("teste123", 10); 
+      } else {
+        throw new Error("Já existem administradores no banco de dados.");
+      }
+    })
+    .then((hashedPassword) => {
+      return Admin.create({ 
+        nome: "Admin Padrão",
+        cpf: 12345678900, 
+        senha: hashedPassword,
+        email: "admin@exemplo.com"
+      });
+    })
+    .then(() => {
+      console.log("Administrador padrão criado com sucesso!");
+    })
+    .catch((error) => {
+      if (error.message === "Já existem administradores no banco de dados.") {
+        console.log(error.message);
+      } else {
+        console.error("Erro ao criar administrador padrão:", error);
+      }
+    });
+}
+
 
 class Database {
   constructor() {
@@ -12,6 +43,7 @@ _connect() {
      mongoose.connect(`mongodb://${server}/${database}`)
        .then(() => {
          console.log('Database connection successful')
+         criarAdminPadrao()
        })
        .catch(err => {
          console.error('Database connection error')
--- backend/route/clienteRoute.js
+++ backend/route/clienteRoute.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const clienteController = require("../controller/clienteController.js");
 const withAuth = require('../controller/middleware/middlewareAuth.js');
-//add withAuth em tudo na fase 2, deixar sem para facilitar os testes
 
-router.get("/cliente", clienteController.getAllClientes);
+router.get("/cliente", withAuth, clienteController.getAllClientes);
 router.post("/cliente", clienteController.postCliente);
-router.get("/cliente/:id", clienteController.getCliente);
-router.delete("/cliente/:id", clienteController.deleteCliente);
-router.put("/cliente/:id",  clienteController.putCliente);
+router.get("/cliente/:id", withAuth, clienteController.getCliente);
+router.delete("/cliente/:id", withAuth, clienteController.deleteCliente);
+router.put("/cliente/:id", withAuth, clienteController.putCliente);
 
 module.exports = router;
